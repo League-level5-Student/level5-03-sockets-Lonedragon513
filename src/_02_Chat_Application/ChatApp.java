3c098c5e3ff3039a8c5e26264ee786e7f1aad78d
lonedragon513 <lonedragon513@192.168.3.122>
2021-04-10 11:26:53 -0700

    a

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,9 +1,22 @@
 package _02_Chat_Application;
 
+import javax.swing.JButton;
+
+import _00_Click_Chat.gui.ButtonClicker;
+import _01_Intro_To_Sockets.client.ClientGreeter;
+import _01_Intro_To_Sockets.server.ServerGreeter;
+
 /*
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
 public class ChatApp {
-
+	JButton button = new JButton("CLICK");
+	
+	ServerGreeter server;
+	ClientGreeter client;
+	public static void main(String[] args) {
+		new ChatApp();
+	}
+	
 }
